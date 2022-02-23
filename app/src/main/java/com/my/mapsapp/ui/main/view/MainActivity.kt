d4334493770ddb2030640e4d4373d947592f2e78
yasherbin <yasherbin@gmail.com>
2022-02-23 15:34:10 +0300

    Add next location displaying
    Added view which shows the user that the location
    list started loading. Added buttons that change the
    annotation displayed.

--- app/src/main/java/com/my/mapsapp/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/my/mapsapp/ui/main/view/MainActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
 import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
@@ -15,6 +16,7 @@ import com.mapbox.geojson.Point
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
 import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
 import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
 import com.my.mapsapp.R
@@ -38,39 +40,64 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val retrofitService = RetrofitBuilder.apiService
         val mainRepository = MainRepository(retrofitService)
-        viewModel = ViewModelProvider(this, ViewModelFactory(mainRepository)).get(MainViewModel::class.java)
+        viewModel =
+            ViewModelProvider(this, ViewModelFactory(mainRepository)).get(MainViewModel::class.java)
         mapView = binding.mapView
+        val annotationApi = mapView?.annotations
+        val pointAnnotationManager = annotationApi?.createPointAnnotationManager()
 
         viewModel.errorMessage.observe(this, {
         })
 
         viewModel.loading.observe(this, Observer {
+            if (it) {
+                binding.loadingTextView.visibility = View.VISIBLE
+                binding.previousButton.visibility = View.GONE
+                binding.nextButton.visibility = View.GONE
+            } else {
+                binding.loadingTextView.visibility = View.GONE
+                binding.previousButton.visibility = View.VISIBLE
+                binding.nextButton.visibility = View.VISIBLE
+            }
         })
 
         viewModel.locationsList.observe(this) { list ->
             mapView?.getMapboxMap()?.loadStyleUri(
                 Style.MAPBOX_STREETS
-            ) { addAnnotationToMap(list) }
+            ) {
+                var index = 0
+                addAnnotationToMap(list[index], pointAnnotationManager)
+                binding.previousButton.setOnClickListener {
+                    if (index == 0) index = list.size - 1
+                    else index--
+                    addAnnotationToMap(list[index], pointAnnotationManager)
+                }
+                binding.nextButton.setOnClickListener {
+                    if (index == list.size - 1) index = 0
+                    else index++
+                    addAnnotationToMap(list[index], pointAnnotationManager)
+                }
+            }
         }
-
         viewModel.getLocations()
     }
 
-    private fun addAnnotationToMap(list: List<Location>) {
-
+    private fun addAnnotationToMap(
+        location: Location,
+        pointAnnotationManager: PointAnnotationManager?
+    ) {
         bitmapFromDrawableRes(
             this@MainActivity,
             R.drawable.red_marker
         )?.let {
-            val annotationApi = mapView?.annotations
-            val pointAnnotationManager = annotationApi?.createPointAnnotationManager(mapView!!)
             val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
-                .withPoint(Point.fromLngLat(list[0].latlng[1], list[0].latlng[0]))
+                .withPoint(Point.fromLngLat(location.latlng[1], location.latlng[0]))
                 .withIconImage(it)
-
+            pointAnnotationManager?.deleteAll()
             pointAnnotationManager?.create(pointAnnotationOptions)
         }
     }
+
     private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
         convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
 
